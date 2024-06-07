a8dbd2d559e72d21f1db51c95514bf5e3e688b88
kajananrajendran <kajananrajendran@localhost>
2024-06-07 22:45:31 +0530

    Second commit

--- Spring-Securtity-sts-2/src/main/java/com/kajanan/SpringSecurtitySts2Application.java
+++ Spring-Securtity-sts-2/src/main/java/com/kajanan/SpringSecurtitySts2Application.java
@@ -2,14 +2,12 @@ package com.kajanan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@Component
-public class SpringSecurtityStsApplication {
+public class SpringSecurtitySts2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurtityStsApplication.class, args);
+		SpringApplication.run(SpringSecurtitySts2Application.class, args);
 	}
 
 }
